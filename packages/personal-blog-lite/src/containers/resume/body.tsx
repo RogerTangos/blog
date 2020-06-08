b940781655b56e321e4f16f030b00536b6b665bb
Albert Carter <albert.r.carter@gmail.com>
2020-06-08 10:36:42 +0200

    successfully import body

--- packages/personal-blog-lite/src/containers/resume/body.tsx
+++ packages/personal-blog-lite/src/containers/resume/body.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const BodyHTML = (props: any) => {
-    <h1>HELLO WORLD</h1>
+const BodyHTML = () => {
+    return(
+        <h1>HELLO WORLD</h1>
+        )
 }
 
 export default BodyHTML
