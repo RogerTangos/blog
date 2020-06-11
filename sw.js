43ac9df4433c2a2e7494e399da71277102bafadc
Albert Carter <albert.r.carter@gmail.com>
2020-06-11 16:46:06 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-80a92c84bc6b150f8fb4.js"
+    "url": "webpack-runtime-7a8c03edc7847c30be10.js"
   },
   {
     "url": "styles.bb4b907a554e42a7ca01.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9ea2737b5fd99eb2f5bf9e4219bc2f63"
+    "revision": "92a04ac9a7095c0d1708e46eda17abba"
   },
   {
     "url": "google-fonts/s/firasans/v10/va9B4kDNxMZdWfMOD5VnLK3eRhf6.woff2",
